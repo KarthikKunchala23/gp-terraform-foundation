abd2149db03d433261273577e62b0a091a83a267
karthik <karthikkunchala2398@gmail.com>
2026-06-07 13:21:05 +0530

    adding new team foundation infra

---
+++ terraform/dev/catalog-app-team/variables.tf
@@ -0,0 +1,14 @@
+variable "team" {
+  type = string
+  default = "catalog-app-team"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "project" {
+  type = string
+  default = "catalog-app"
+}
