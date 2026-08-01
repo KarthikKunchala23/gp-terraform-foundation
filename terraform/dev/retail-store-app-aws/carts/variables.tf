99aa2b6c79b112c35b8eb911bf0904c0b00323a3
karthik <karthikkunchala2398@gmail.com>
2026-08-01 17:38:26 +0530

    added dynamodb child module with example root module

---
+++ terraform/dev/retail-store-app-aws/carts/variables.tf
@@ -0,0 +1,11 @@
+variable "team" {
+  description = "Team Name for the app"
+  type        = string
+  default     = "carts"
+}
+
+variable "environment" {
+  description = "Environment Name for the app"
+  type        = string
+  default     = "dev"
+}
